39fd71bebaa70aaea20fa845ef64da575ce2abcd
jitendra <jitendra.pal@snapdeal.com>
2021-08-05 17:43:50 +0530

    added unload event

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,7 +2,21 @@ import Common from '../../components/common/common';
 import AppHeader from '../../components/header';
 import Head from 'next/head'
 import AppFooter from '../../components/appFooter';
+import { useEffect } from 'react';
 export default function AboutUs(){
+    
+    const checkTabClose = ()=>{
+        console.log("attached Closing");
+        window.addEventListener('beforeunload', function (e) {
+            // e.preventDefault();
+            
+            alert("tab closing");
+        });
+      }
+      useEffect(()=>{
+          alert("hi")
+        checkTabClose();
+      },[])
     return (
         <div className="container">
             <Common/>
