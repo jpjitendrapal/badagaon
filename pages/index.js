32ae8beba30f99be3e0a429c9806edafbb0d1a66
jitendra <jitendra.pal@snapdeal.com>
2021-11-12 13:34:39 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,38 @@ import Link from 'next/link';
 import AppHeader from './../components/header';
 import AppFooter from './../components/appFooter';
 import Common from './../components/common/common';
+// import LazyImg from 'react-img-lazy';
 import ImageGrid from '../components/imageGrid/grid';
+import ImageGallery from 'react-image-gallery';
+import style from 'react-image-gallery/styles/css/image-gallery.css';
+// import './home.module.scss';
 
 export default function Home() {
+  const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+  ];
   return (
     <div className="container">
-      
       <AppHeader/>
       <main>
         {/* <ImageGrid images={[{url:"",alt:"alt1"},{url:"", alt:"alt2"}]} /> */}
         <h1 className="title">
           Welcome to Badagaon
+          
+          {/* <LazyImg/> */}
         </h1>
+        <ImageGallery items={images} />
       </main>
       <AppFooter/>
       <Common/>
@@ -23,17 +43,16 @@ export default function Home() {
           max-width: 25px;
         }
         main {
-          padding: 4rem 0;
+          padding: 0.5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
           text-align: center;
-          margin-top: 3rem;
         }
         .title{
-          height: 300px;
+          height: auto;
           font-size: 3rem;
         }
         .title a {
@@ -46,9 +65,6 @@ export default function Home() {
           .grid {
             width: 100%;
             flex-direction: column;
-          }
-          .title{
-            height: 250px;
           }
         }
       `}</style>
--- pages/post/[pid].js
+++ pages/post/[pid].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import AppHeader from './../../components/header';
+import AppFooter from './../../components/appFooter';
 import Common from '../../components/common/common';
 
 const Post = () => {
@@ -9,7 +10,10 @@ const Post = () => {
   return (<div>
             <Common/>
             <AppHeader/>
-            <p>Post: {pid}</p>
+            <div style={{height: '6100px'}}>
+              <p>Post: {pid}</p>
+            </div>
+            <AppFooter/>
           </div>)
 }
 
